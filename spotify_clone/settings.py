e9b800a35368fd5b1a067adccf9d86b2a2c45833
Asna Zulfiqar <asna.zulfiqar018@gmail.com>
2025-02-27 17:46:21 +0500

    Added Databse

--- spotify_clone/settings.py
+++ spotify_clone/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+env_file = os.path.join(BASE_DIR, ".env")
+env = environ.Env()
+env.read_env(env_file)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6zjfqwdyr0!$!*53eo213u+b%lc$l4bj3#y+ohn$-l--6=b=$!'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
+
 
 ALLOWED_HOSTS = []
 
@@ -75,11 +82,15 @@ WSGI_APPLICATION = 'spotify_clone.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+if env.str("DATABASE_URL", default=None):
+    DATABASES = {"default": env.db()}
+
 
 
 # Password validation
